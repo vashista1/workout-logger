1653e0ff224438cb1d508ed0525ca294f441a992
Vashista Gande <vashistagande@gmail.com>
2019-08-07 10:58:53 -0400

    Initial Commit

---
+++ workout_logger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workout_logger
+//
+//  Created by Vashista Gande on 2019-08-07.
+//  Copyright © 2019 Vashista Gande. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
